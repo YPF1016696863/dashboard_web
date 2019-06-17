23ebbaf06537e2f084d035053fb305ca2b5ad471
Yang Yang <yangyangwsws@gmail.com>
2019-06-17 23:44:12 +0200

    adjust eslint rules and prettier

--- client/app/pages/dashboards/DashboardList.jsx
+++ client/app/pages/dashboards/DashboardList.jsx
@@ -6,18 +6,25 @@ import { PageHeader } from '@/components/PageHeader';
 import { Paginator } from '@/components/Paginator';
 import { DashboardTagsControl } from '@/components/tags-control/TagsControl';
 
-import { wrap as itemsList, ControllerType } from '@/components/items-list/ItemsList';
+import {
+  wrap as itemsList,
+  ControllerType
+} from '@/components/items-list/ItemsList';
 import { ResourceItemsSource } from '@/components/items-list/classes/ItemsSource';
 import { UrlStateStorage } from '@/components/items-list/classes/StateStorage';
 
 import LoadingState from '@/components/items-list/components/LoadingState';
 import * as Sidebar from '@/components/items-list/components/Sidebar';
-import ItemsTable, { Columns } from '@/components/items-list/components/ItemsTable';
+import ItemsTable, {
+  Columns
+} from '@/components/items-list/components/ItemsTable';
 
 import Layout from '@/components/layouts/ContentWithSidebar';
 
 import { Dashboard } from '@/services/dashboard';
 import { routesToAngularRoutes } from '@/lib/utils';
+
+import { appSettingsConfig } from '@/config/app-settings';
 
 import DashboardListEmptyState from './DashboardListEmptyState';
 
@@ -26,51 +33,63 @@ import './dashboard-list.css';
 class DashboardList extends React.Component {
   static propTypes = {
     controller: ControllerType.isRequired,
-    $translate: PropTypes.func,
+    $translate: PropTypes.func
   };
 
   static defaultProps = {
-    $translate: text => text,
+    $translate: text => text
   };
 
   sidebarMenu = [
     {
       key: 'all',
       href: 'dashboards',
-      title: 'SIDEBAR.ALL_DASHBOARDS',
+      title: 'SIDEBAR.ALL_DASHBOARDS'
     },
     {
       key: 'favorites',
       href: 'dashboards/favorites',
       title: 'SIDEBAR.FAVORITES',
-      icon: () => <Sidebar.MenuIcon icon="fa fa-star" />,
-    },
+      icon: () => <Sidebar.MenuIcon icon="fa fa-star" />
+    }
   ];
 
   listColumns = [
     Columns.favorites({ className: 'p-r-0' }),
-    Columns.custom.sortable((text, item) => (
-      <React.Fragment>
-        <a className="table-main-title" href={'dashboard/' + item.slug} data-test={item.slug}>{ item.name }</a>
-        <DashboardTagsControl
-          className="d-block"
-          tags={item.tags}
-          isDraft={item.is_draft}
-          isArchived={item.is_archived}
-        />
-      </React.Fragment>
-    ), {
-      title: 'Name',
-      field: 'name',
-      width: null,
-    }),
-    Columns.avatar({ field: 'user', className: 'p-l-0 p-r-0' }, name => `Created by ${name}`),
+    Columns.custom.sortable(
+      (text, item) => (
+        <React.Fragment>
+          <a
+            className="table-main-title"
+            href={'dashboard/' + item.slug}
+            data-test={item.slug}
+          >
+            {item.name}
+          </a>
+          <DashboardTagsControl
+            className="d-block"
+            tags={item.tags}
+            isDraft={item.is_draft}
+            isArchived={item.is_archived}
+          />
+        </React.Fragment>
+      ),
+      {
+        title: 'Name',
+        field: 'name',
+        width: null
+      }
+    ),
+    Columns.avatar(
+      { field: 'user', className: 'p-l-0 p-r-0' },
+      name => `Created by ${name}`
+    ),
     Columns.dateTime.sortable({
       title: 'Created At',
       field: 'created_at',
       className: 'text-nowrap',
-      width: '1%',
-    }),
+      width: '1%'
+    })
   ];
 
   render() {
@@ -86,13 +105,22 @@ class DashboardList extends React.Component {
               value={controller.searchTerm}
               onChange={controller.updateSearch}
             />
-            <Sidebar.Menu items={this.sidebarMenu} selected={controller.params.currentPage} $translate={translate} />
-            <Sidebar.Tags url="api/dashboards/tags" onChange={controller.updateSelectedTags} />
+            <Sidebar.Menu
+              items={this.sidebarMenu}
+              selected={controller.params.currentPage}
+              $translate={translate}
+            />
+            <Sidebar.Tags
+              url={appSettingsConfig.server.backendUrl + '/api/dashboards/tags'}
+              onChange={controller.updateSelectedTags}
+            />
             <Sidebar.PageSizeSelect
               className="m-b-10"
               options={controller.pageSizeOptions}
               value={controller.itemsPerPage}
-              onChange={itemsPerPage => controller.updatePagination({ itemsPerPage })}
+              onChange={itemsPerPage =>
+                controller.updatePagination({ itemsPerPage })
+              }
             />
           </Layout.Sidebar>
           <Layout.Content>
@@ -118,7 +146,11 @@ class DashboardList extends React.Component {
                       totalCount={controller.totalItemsCount}
                       itemsPerPage={controller.itemsPerPage}
                       page={controller.page}
-                      onChange={page => controller.updatePagination({ page })}
+                      onChange={page =>
+                        controller.updatePagination({
+                          page
+                        })
+                      }
                     />
                   </div>
                 )}
@@ -134,42 +166,56 @@ class DashboardList extends React.Component {
 }
 
 export default function init(ngModule) {
-  ngModule.component('pageDashboardList', react2angular(itemsList(
-    DashboardList,
-    new ResourceItemsSource({
-      getResource({ params: { currentPage } }) {
-        return {
-          all: Dashboard.query.bind(Dashboard),
-          favorites: Dashboard.favorites.bind(Dashboard),
-        }[currentPage];
-      },
-      getItemProcessor() {
-        return (item => new Dashboard(item));
-      },
-    }),
-    new UrlStateStorage({ orderByField: 'created_at', orderByReverse: true }),
-  ), [], ['$translate']));
+  ngModule.component(
+    'pageDashboardList',
+    react2angular(
+      itemsList(
+        DashboardList,
+        new ResourceItemsSource({
+          getResource({ params: { currentPage } }) {
+            return {
+              all: Dashboard.query.bind(Dashboard),
+              favorites: Dashboard.favorites.bind(Dashboard)
+            }[currentPage];
+          },
+          getItemProcessor() {
+            return item => new Dashboard(item);
+          }
+        }),
+        new UrlStateStorage({
+          orderByField: 'created_at',
+          orderByReverse: true
+        })
+      ),
+      [],
+      ['$translate']
+    )
+  );
 
-  return routesToAngularRoutes([
+  return routesToAngularRoutes(
+    [
+      {
+        path: '/dashboards',
+        title: 'Dashboards',
+        key: 'all'
+      },
+      {
+        path: '/dashboards/favorites',
+        title: 'Favorite Dashboards',
+        key: 'favorites'
+      }
+    ],
     {
-      path: '/dashboards',
-      title: 'Dashboards',
-      key: 'all',
-    },
-    {
-      path: '/dashboards/favorites',
-      title: 'Favorite Dashboards',
-      key: 'favorites',
-    },
-  ], {
-    reloadOnSearch: false,
-    template: '<page-dashboard-list on-error="handleError"></page-dashboard-list>',
-    controller($scope, $exceptionHandler) {
-      'ngInject';
+      reloadOnSearch: false,
+      template:
+        '<page-dashboard-list on-error="handleError"></page-dashboard-list>',
+      controller($scope, $exceptionHandler) {
+        'ngInject';
 
-      $scope.handleError = $exceptionHandler;
-    },
-  });
+        $scope.handleError = $exceptionHandler;
+      }
+    }
+  );
 }
 
 init.init = true;
