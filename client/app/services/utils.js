af04c63bd52988a49620706bcb60622cd4292e0e
Wenqing Gu <wenqing.gu@gmail.com>
2019-08-14 03:34:04 +0200

    Adjust dynamic settings

--- client/app/services/utils.js
+++ client/app/services/utils.js
@@ -1,7 +1,31 @@
 // eslint-disable-next-line import/prefer-default-export
 export function absoluteUrl(url) {
-  const urlObj = new URL(url, window.location);
-  urlObj.protocol = window.location.protocol;
-  urlObj.host = window.location.host;
-  return urlObj.toString();
+	const DOMAIN_TO_REPLACE = "DOMAIN_TO_REPLACE";
+	let activate = "http://datavis-login." + DOMAIN_TO_REPLACE + "/active";
+
+	function checkIsIPV4(entry) {
+		const blocks = entry.split(".");
+		if (blocks.length === 4) {
+			return blocks.every(function (block) {
+				return parseInt(block, 10) >= 0 && parseInt(block, 10) <= 255;
+			});
+		}
+		return false;
+	}
+
+	const hostname = window.location.hostname;
+
+	if (hostname.startsWith("localhost") ||
+		hostname.startsWith("docker-internal") ||
+		checkIsIPV4(hostname)) {
+		activate = "http://" + hostname + ":8092/active";
+	} else {
+		const blocks = hostname.split(".");
+		const primaryBlocks = blocks.length >= 3 ? blocks.slice(1) : blocks;
+		const primary = primaryBlocks.join(".");
+
+		activate = activate.replace(DOMAIN_TO_REPLACE, primary);
+	}
+
+	return activate + "?token=" + (url.replace("/reset/", "").replace("/invite/", ""));
 }
