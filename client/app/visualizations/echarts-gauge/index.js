874666ce6440be5c11d55569542498639329be51
yuhao <496659850@qq.com>
2019-12-13 20:44:15 +0800

    feature( ) 优化组件选择2

--- client/app/visualizations/echarts-gauge/index.js
+++ client/app/visualizations/echarts-gauge/index.js
@@ -17,96 +17,101 @@ function EchartsGaugeRenderer($rootScope) {
     template: echartsTemplate,
     link($scope, $element) {
 
-      
-      
+
+
       if (_.isEmpty($scope.options) || $scope.options.chartType !== "GaugeChart") {
         $scope.options = defaultGaugeChartOptions();
       }
       console.log($scope.options);
+
       const refreshData = () => {
-        if (!_.isUndefined($scope.queryResult) && $scope.queryResult.getData()) {
-          const data = $scope.queryResult.getData();
-          let dataGauge = 0;
-          // eslint-disable-next-line func-names
-          _.forEach(data, function (value) {// [{0},{1}...] 筛选出每一个{0} {1} ...
+        try {
+          if (!_.isUndefined($scope.queryResult) && $scope.queryResult.getData()) {
+            const data = $scope.queryResult.getData();
+            let dataGauge = 0;
             // eslint-disable-next-line func-names
-            _.forEach(value, function (valueChildren, keyChildren) {
-              if (keyChildren === _.get($scope.options, "form.xAxisColumn", '')) {
-                dataGauge = valueChildren;
-              }
+            _.forEach(data, function (value) {// [{0},{1}...] 筛选出每一个{0} {1} ...
+              // eslint-disable-next-line func-names
+              _.forEach(value, function (valueChildren, keyChildren) {
+                if (keyChildren === _.get($scope.options, "form.xAxisColumn", '')) {
+                  dataGauge = valueChildren;
+                }
 
+              });
             });
-          });
 
-          // 切换主题颜色
-          setThemeColor($scope.options, _.get($rootScope, "theme.theme", "light"));
+            // 切换主题颜色
+            setThemeColor($scope.options, _.get($rootScope, "theme.theme", "light"));
 
-          _.set($scope.options, "series", []);// 清空设置           
-          $scope.options.series.push({
-            name: _.get($scope.options, "series_Name", ''),
-            type: 'gauge',
-            min: _.get($scope.options, "minValue", 0),
-            max: _.get($scope.options, "maxValue", 100),
-            detail: {
-              color: _.get($scope.options, "zbColor", 'auto'),
-              formatter: _.get($scope.options, "defValue", true) ? '{value}' : '{value}%',
-            },
-            splitNumber: _.get($scope.options, "splitNumber", 10),// 仪表盘刻度的分割段数。
-            itemStyle: {
-              color: _.get($scope.options, "zColor", 'auto'),// 指针颜色
-            },
-            axisLine: {
-              lineStyle: {
-                color: [
-                  [_.get($scope.options, "duan1", 0.3), _.get($scope.options, "duan1Color", '#01c7ae')],
-                  [_.get($scope.options, "duan2", 0.5), _.get($scope.options, "duan2Color", '#03869e')],
-                  [_.get($scope.options, "duan3", 0.8), _.get($scope.options, "duan3Color", '#d09931')],
-                  [_.get($scope.options, "duan4", 1), _.get($scope.options, "duan4Color", '#c23531')]
-                ],
-              }
-            },
+            _.set($scope.options, "series", []);// 清空设置           
+            $scope.options.series.push({
+              name: _.get($scope.options, "series_Name", ''),
+              type: 'gauge',
+              min: _.get($scope.options, "minValue", 0),
+              max: _.get($scope.options, "maxValue", 100),
+              detail: {
+                color: _.get($scope.options, "zbColor", 'auto'),
+                formatter: _.get($scope.options, "defValue", true) ? '{value}' : '{value}%',
+              },
+              splitNumber: _.get($scope.options, "splitNumber", 10),// 仪表盘刻度的分割段数。
+              itemStyle: {
+                color: _.get($scope.options, "zColor", 'auto'),// 指针颜色
+              },
+              axisLine: {
+                lineStyle: {
+                  color: [
+                    [_.get($scope.options, "duan1", 0.3), _.get($scope.options, "duan1Color", '#01c7ae')],
+                    [_.get($scope.options, "duan2", 0.5), _.get($scope.options, "duan2Color", '#03869e')],
+                    [_.get($scope.options, "duan3", 0.8), _.get($scope.options, "duan3Color", '#d09931')],
+                    [_.get($scope.options, "duan4", 1), _.get($scope.options, "duan4Color", '#c23531')]
+                  ],
+                }
+              },
 
-            data: [{
-              value: setData($scope.options, _.get($scope.options, "defValue", true), dataGauge),
-              // dataGauge / (_.get($scope.options, "maxValue", 100) - _.get($scope.options, "minValue", 0))
-              name: _.get($scope.options, "series_Name", '') === '' || undefined ?
-                _.get($scope.options, "form.xAxisColumn", '') :
-                _.get($scope.options, "series_Name", true)
-            }],
-          });
+              data: [{
+                value: setData($scope.options, _.get($scope.options, "defValue", true), dataGauge),
+                // dataGauge / (_.get($scope.options, "maxValue", 100) - _.get($scope.options, "minValue", 0))
+                name: _.get($scope.options, "series_Name", '') === '' || undefined ?
+                  _.get($scope.options, "form.xAxisColumn", '') :
+                  _.get($scope.options, "series_Name", true)
+              }],
+            });
 
-          let myChart = null;
+            let myChart = null;
 
-          if (document.getElementById("gauge-main")) {
-            document.getElementById("gauge-main").id = $scope.options.id;
-            // eslint-disable-next-line
-            myChart = echarts.init(document.getElementById($scope.options.id));
-          } else {
-            // eslint-disable-next-line
-            myChart = echarts.init(document.getElementById($scope.options.id));
-          }
-
-          if (_.get($scope.options, "form.isCodeEnabled", false)) {
-            myChart.setOption(JSON.parse(_.replace($scope.options.form.code, "'", '"')), true);
-          } else {
-            myChart.setOption($scope.options, true);
-          }
-          // Resize - Responsive
-          if (_.get($scope.options, "size.responsive", true)) {
-
-            // Find widget and resize
-            let height = "100%";
-            if ($($element[0]).closest('.widget-container').length === 0) {
-              // Set a default height for widget.
-              height = "400px";
+            if (document.getElementById("gauge-main")) {
+              document.getElementById("gauge-main").id = $scope.options.id;
+              // eslint-disable-next-line
+              myChart = echarts.init(document.getElementById($scope.options.id));
+            } else {
+              // eslint-disable-next-line
+              myChart = echarts.init(document.getElementById($scope.options.id));
             }
-            _.set($scope.options, "size", {
-              responsive: true,
-              width: Math.floor($element.parent().width()) + "px",
-              height
-            });
+
+            if (_.get($scope.options, "form.isCodeEnabled", false)) {
+              myChart.setOption(JSON.parse(_.replace($scope.options.form.code, "'", '"')), true);
+            } else {
+              myChart.setOption($scope.options, true);
+            }
+            // Resize - Responsive
+            if (_.get($scope.options, "size.responsive", true)) {
+
+              // Find widget and resize
+              let height = "100%";
+              if ($($element[0]).closest('.widget-container').length === 0) {
+                // Set a default height for widget.
+                height = "400px";
+              }
+              _.set($scope.options, "size", {
+                responsive: true,
+                width: Math.floor($element.parent().width()) + "px",
+                height
+              });
+            }
+            myChart.resize($scope.options.size.width, $scope.options.size.height);
           }
-          myChart.resize($scope.options.size.width, $scope.options.size.height);
+        } catch (e) {
+          console.log("先选组件类型 则该方法不存在因此用trycatch来解决 ：$scope.queryResult.getData is not a function");
         }
       };
 
@@ -127,10 +132,12 @@ function EchartsGaugeEditor() {
       options: '=?',
     },
     link($scope) {
-
-      $scope.columns = $scope.queryResult.getColumns();
-      $scope.columnNames = _.map($scope.columns, i => i.name);
-
+      try {
+        $scope.columns = $scope.queryResult.getColumns();
+        $scope.columnNames = _.map($scope.columns, i => i.name);
+      } catch (e) {
+        console.log("先选组件类型 则该方法不存在因此用trycatch来解决 ：$scope.queryResult.getData is not a function");
+      }
       // Set default options for new vis// 20191203 bug fix 
       if (_.isEmpty($scope.options) || $scope.options.chartType !== "GaugeChart") {
         $scope.options = defaultGaugeChartOptions();
